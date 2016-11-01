7556680129244e865b7f0d1611f0adb998112c11
Kaustav Majumder <pupunmajumder@gmail.com>
2016-11-01 19:37:57 +0530

    setting the backend

---
+++ backend/server.js
@@ -0,0 +1,21 @@
+var express = require("express");
+var bodyparser = require("body-parser");
+var app = express();
+
+app.use(bodyparser.json());
+
+app.use(function(req,res,next){
+	res.header("Access-Control-Allow-Origin", "*");
+	res.header("Access-Control-Allow-Headers", "Content-type , Authorization");
+	next();
+});
+app.post('/api/message',function(req,res){
+	console.log(req.body);
+	res.status(200);
+	res.send({message : "Posted Successfully"});
+});
+
+
+var server = app.listen(5000,function(){
+	console.log("Listeninf on ",server.address().port);
+});
